fe0ffabbebd324a1555b8a496699c3eb5c630eb7
grn31 <green3131@163.com>
2018-07-24 21:28:08 +0800

    learn template

--- zqxt_tmpl/zqxt_tmpl/urls.py
+++ zqxt_tmpl/zqxt_tmpl/urls.py
@@ -1,4 +1,4 @@
-"""zqxt_views URL Configuration
+"""zqxt_tmpl URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from calc import views as calc_views                # 导入calc应用中的views文件，为防止文件名冲突使用as重命名
+from learn import views as learn_views          # 导入learn应用中的views文件，为防止文件名冲突使用as重命名
 
 urlpatterns = [
-    url(r'^$', calc_views.index, name='home'),      # 添加index()函数, 对应URL为/index.html
-    url(r'^add/$', calc_views.add, name='add'),     # 添加calc_views中的add()函数, 对应URL为^add/$
-  # url(r'^add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),   # (\d+) = 一个或多个数字
-    url(r'^add/(\d+)/(\d+)/$', calc_views.old_add2_redirect),   # 转换url, 并重定向
-    url(r'^new_add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),
+    url(r'^$', learn_views.home, name='home'),  # 添加learn.home()函数
     url(r'^admin/', admin.site.urls),
 ]
