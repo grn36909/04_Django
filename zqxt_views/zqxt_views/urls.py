9bf7d20933d92c61b3851db1d50d64b9c7edbff1
grn31 <green3131@163.com>
2018-07-24 17:46:23 +0800

    correct execution

--- zqxt_views/zqxt_views/urls.py
+++ zqxt_views/zqxt_views/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from calc import views as calc_views                # 导入calc应用中的views文件，为防止文件名冲突使用as重命名
 
 urlpatterns = [
+    url(r'^$', calc_views.index, name='home'),      # 添加index()函数, 对应URL为/index.html
     url(r'^add/$', calc_views.add, name='add'),     # 添加calc_views中的add()函数, 对应URL为^add/$
-    url(r'^add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),   # (\d+) = 一个或多个数字
+  # url(r'^add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),   # (\d+) = 一个或多个数字
+    url(r'^add/(\d+)/(\d+)/$', calc_views.old_add2_redirect),   # 转换url, 并重定向
+    url(r'^new_add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),
     url(r'^admin/', admin.site.urls),
 ]
