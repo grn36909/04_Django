814efc6bfab71441bdb38dd93615da60f14bbf75
grn31 <green3131@163.com>
2018-07-25 14:38:12 +0800

    e05 template2

--- zqxt_tmpl/zqxt_tmpl/urls.py
+++ zqxt_tmpl/zqxt_tmpl/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from learn import views as learn_views          # 导入learn应用中的views文件，为防止文件名冲突使用as重命名
 
 urlpatterns = [
-    url(r'^$', learn_views.home, name='home'),  # 添加learn.home()函数
+    url(r'^$', learn_views.home_text, name='home_text'),        # 添加learn.home_text()函数
+    url(r'^for/', learn_views.home_for, name='home_for'),       # 添加learn.home_for()函数
+    url(r'^dict/', learn_views.home_dict, name='home_dict'),    # 添加learn.home_dict()函数
+    url(r'^list/', learn_views.home_list, name='home_list'),    # 添加learn.home_list()函数
     url(r'^admin/', admin.site.urls),
 ]
