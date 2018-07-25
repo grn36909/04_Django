1fe0193f1059ccd474c3cc808298d8e12bdfc7b1
grn31 <green3131@163.com>
2018-07-25 19:22:40 +0800

    e06-models

--- learn_models/learn_models/urls.py
+++ learn_models/learn_models/urls.py
@@ -1,4 +1,4 @@
-"""zqxt_tmpl URL Configuration
+"""learn_models URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,12 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from learn import views as learn_views          # 导入learn应用中的views文件，为防止文件名冲突使用as重命名
 
 urlpatterns = [
-    url(r'^$', learn_views.home_text, name='home_text'),        # 添加learn.home_text()函数
-    url(r'^for/', learn_views.home_for, name='home_for'),       # 添加learn.home_for()函数
-    url(r'^dict/', learn_views.home_dict, name='home_dict'),    # 添加learn.home_dict()函数
-    url(r'^list/', learn_views.home_list, name='home_list'),    # 添加learn.home_list()函数
     url(r'^admin/', admin.site.urls),
 ]
